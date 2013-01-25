3a6c79fbe308931397edfad8b756e53b0f60395a
Kamil Mrowiec <kaamilm@gmail.com>
2013-01-25 16:09:43 +0000

    working udp test code

--- python/udp_test/test_udp.py
+++ python/udp_test/test_udp.py
@@ -1,26 +1,24 @@
-from socket import *
-import sys, time
+import socket, traceback
+import time
 
-host = "localhost"
-textport = "4321"
+host = ''                               # Bind to all interfaces
+port = 4321
 
-#s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-port = int(textport)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.bind((host, port))
 
-s = socket(AF_INET, SOCK_DGRAM)
-s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-#s.sendto('This is a test', ('255.255.255.255', 54545))
-s.connect(('<broadcast>', 4321)) 
+message = "lat=52.4064 long=-4.0764 spd=1.2000 bhead=90 thead=90 whead=90 wind=90 spos=128 rpos=130 nwn=2 nwlat=52.4064 nwlon=-4.0779 time=1336476465"
 
-print "Enter data to transmit: "
-data = sys.stdin.readline().strip()
-s.sendall(data)
-s.shutdown(1)
-
-print "Looking for replies; press Ctrl-C or Ctrl-Break to stop."
 while 1:
-    buf = s.recv(2048)
-    if not len(buf):
-        break
-    print "Received: %s" % buf
+    try:
+        #message, address = s.recvfrom(8192)
+        #print "Got data from", address
+        # Acknowledge it.
+        s.sendto("I am here", ("<broadcast>", 4321))
+	time.sleep(2)
+    except (KeyboardInterrupt, SystemExit):
+        raise
+    except:
+        traceback.print_exc()
