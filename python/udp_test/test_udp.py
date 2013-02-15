bb2fe9d46f7632dcea777cc9650804bb7e6d10ac
Kamil Mrowiec <kaamilm@gmail.com>
2013-02-15 02:07:45 +0000

    Improved test UDP packets sender

--- python/udp_test/test_udp.py
+++ python/udp_test/test_udp.py
@@ -1,23 +1,25 @@
 import socket, traceback
 import time
 
-host = ''                               # Bind to all interfaces
-port = 4321
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 s.bind((host, port))
 
-message = "lat=52.4064 long=-4.0764 spd=1.2000 bhead=90 thead=90 whead=90 wind=90 spos=128 rpos=130 nwn=2 nwlat=52.4064 nwlon=-4.0779 time=1336476465"
+#message = "lat=52.4064 long=-4.0764 spd=1.2000 bhead=90 thead=90 whead=90 wind=90 spos=128 rpos=130 nwn=2 nwlat=52.4064 nwlon=-4.0779 time=1336476465"
+rest=" spd=1.2000 bhead=90 thead=90 whead=90 wind=90 spos=128 rpos=130 nwn=2 nwlat=52.4064 nwlon=-4.0779"
+lat=52.41156
+lon=-4.08975
 
 while 1:
     try:
-        #message, address = s.recvfrom(8192)
-        #print "Got data from", address
-        # Acknowledge it.
-        s.sendto("I am here", ("<broadcast>", 4321))
+      	message="lat="+str(lat)+" lon="+str(lon)+rest+" time="+str(time.time())
+	print message 
+        s.sendto(message, ("<broadcast>", 4321))
 	time.sleep(2)
+	lat+=0.00005
+	lon-=0.00005
     except (KeyboardInterrupt, SystemExit):
         raise
     except:
