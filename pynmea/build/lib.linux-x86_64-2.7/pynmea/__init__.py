dee42c0dee0f00704a5a5ebf2dc7df8cea158c32
Louis Taylor <kragniz@gmail.com>
2013-03-04 00:38:33 +0000

    Moved pynmea into the python directory

--- pynmea/build/lib.linux-x86_64-2.7/pynmea/__init__.py
+++
@@ -1,3 +0,0 @@
-__VERSION__ =  (0, 3, 0)
-def get_version():
-    return '.'.join([str(x) for x in __VERSION__])
--- pynmea/build/lib.linux-x86_64-2.7/pynmea/exceptions.py
+++
@@ -1,6 +0,0 @@
-""" Collection of pynmea specific errors
-"""
-
-class NoDataGivenError(Exception):
-    def __init__(self, message):
-        self.message = message
