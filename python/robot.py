3775e0d9b29ec22801ab3ad060359ca286ea9d3f
Louis Taylor <kragniz@gmail.com>
2013-03-05 18:26:32 +0000

    Added small logging method to Robot

--- python/robot.py
+++ python/robot.py
@@ -1,5 +1,16 @@
 from arduino import Arduino
+import time
 
 class Robot(object):
     def __init__(self):
-        pass
+        self._arduino = Arduino()
+
+    def log(self):
+        '''Log the output of most of the sensors'''
+        l = 'Time: {time}, Heading: {head}, Wind: {wind}\n'.format(
+                time = time.asctime(),
+                head = self._arduino.getCompass()
+                wind = self._arduino.getCompass()
+            )
+        with open(logfile, 'a') as f:
+            f.writeline(l)
