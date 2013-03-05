44b5455095b25d76ebbdc3fd5e325fe220bdad19
Louis Taylor <kragniz@gmail.com>
2013-03-05 18:50:03 +0000

    Added GPS methods

--- python/robot.py
+++ python/robot.py
@@ -1,16 +1,63 @@
 from arduino import Arduino
 import time
+from pynmea.streamer import NMEAStream
+from pynmea import nmea
+from pynmea.nmea import NMEASentence
+import serial
+import math
 
 class Robot(object):
     def __init__(self):
         self._arduino = Arduino()
+        self._serialport = serial.Serial("/dev/ttyUSB0", 4800, timeout=0.5)
+
+        parse_map = (("Latitude" , "lat"),
+                     ("Direction", "lat_dir"),
+                     ("Longitude", "lon"),
+                     ("Direction", "lon_dir"))
+
+        self.nmea = NMEASentence(parse_map)
+        self.streamer = NMEAStream()
 
     def log(self):
         '''Log the output of most of the sensors'''
-        l = 'Time: {time}, Heading: {head}, Wind: {wind}\n'.format(
+        l = 'Time: {time}, Heading: {head}, Wind: {wind}, Coords: {coords}\n'.format(
                 time = time.asctime(),
-                head = self._arduino.getCompass()
-                wind = self._arduino.getCompass()
+                head = self._arduino.getCompass(),
+                wind = self._arduino.getCompass(),
+                coords = self.getCoords()
             )
-        with open(logfile, 'a') as f:
-            f.writeline(l)
+        with open('logfile', 'a') as f:
+            f.write(l)
+
+    def getCoords(self):
+        response = self._serialport.readline(None)
+        try:
+            data_obs = self.streamer.get_objects(data=response)
+            data_obs += self.streamer.get_objects(data='')
+
+            if len(data_obs) > 0:
+                if data_obs[0].sen_type == "GPGGA":
+                    if data_obs[0].check_chksum():
+                        location = self._getGGA(data_obs[0])
+                        return location
+                    else:
+                        print "checksum failed"
+        except Exception, e:
+            print e
+
+    def _getGGA(self, nmeaOb):
+        lat_deg = float(nmeaOb.latitude[:2])
+        lat_min = float(nmeaOb.latitude[2:9])
+        lat_min_dec = lat_min/60
+        lat = lat_deg + lat_min_dec
+        if nmeaOb.lat_direction == "S":
+            lat = -lat
+    
+        lon_deg = float(nmeaOb.longitude[:3])
+        lon_min = float(nmeaOb.longitude[3:10])
+        lon_min_dec = lon_min/60
+        lon = lon_deg + lon_min_dec
+        if nmeaOb.lon_direction == "W":
+            lon = -lon
+        return lon, lat
