de70cb50c44ee96534a27f1af5712d180af2ce4b
Louis Taylor <kragniz@gmail.com>
2013-03-07 14:57:49 +0000

    Renamed Robot to Boat.
    GetCoords() now returns (-1, -1) when an error occurred

---
+++ python/boat.py
@@ -0,0 +1,69 @@
+from arduino import Arduino
+import config
+
+from pynmea.streamer import NMEAStream
+from pynmea import nmea
+from pynmea.nmea import NMEASentence
+import serial
+import math
+import time
+
+class Boat(object):
+    def __init__(self):
+        self._arduino = Arduino()
+        self._gpsSerial = serial.Serial(config.gpsSerialport, 4800, timeout=0.5)
+
+        parse_map = (('Latitude' , 'lat'),
+                     ('Direction', 'lat_dir'),
+                     ('Longitude', 'lon'),
+                     ('Direction', 'lon_dir'))
+
+        self.nmea = NMEASentence(parse_map)
+        self.streamer = NMEAStream()
+
+    def log(self):
+        """Log the output of most of the sensors"""
+        l = 'Time: {time}, Heading: {head}, Wind: {wind}, Coords: {coords}\n'.format(
+                time = time.asctime(),
+                head = self._arduino.getCompass(),
+                wind = self._arduino.getWind(),
+                coords = self.getCoords()
+            )
+        with open('logfile', 'a') as f:
+            f.write(l)
+
+    def getCoords(self):
+        """Return the current coordinates from the GPS"""
+        response = self._gpsSerial.readline(None)
+        try:
+            data_obs = self.streamer.get_objects(data=response)
+            data_obs += self.streamer.get_objects(data='')
+
+            if len(data_obs) > 0:
+                if data_obs[0].sen_type == "GPGGA":
+                    if data_obs[0].check_chksum():
+                        location = self._getGGA(data_obs[0])
+                        return location
+                    else:
+                        print "checksum failed"
+                        return (-1, -1)
+        except Exception, e:
+            print e
+            return (-1, -1)
+
+    def _getGGA(self, nmeaOb):
+        """Return the coordinates from a nmea object"""
+        lat_deg = float(nmeaOb.latitude[:2])
+        lat_min = float(nmeaOb.latitude[2:9])
+        lat_min_dec = lat_min/60
+        lat = lat_deg + lat_min_dec
+        if nmeaOb.lat_direction == "S":
+            lat = -lat
+    
+        lon_deg = float(nmeaOb.longitude[:3])
+        lon_min = float(nmeaOb.longitude[3:10])
+        lon_min_dec = lon_min/60
+        lon = lon_deg + lon_min_dec
+        if nmeaOb.lon_direction == "W":
+            lon = -lon
+        return lon, lat
