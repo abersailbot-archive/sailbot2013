a892701b4ad184644205dfb4824ee0dc24f38252
Louis Taylor <kragniz@gmail.com>
2013-03-08 20:38:41 +0000

    Added some docstrings to Point

--- sailbot/point.py
+++ sailbot/point.py
@@ -5,6 +5,7 @@ from math import cos as cos
 EARTH_RADIUS = 6371009 #in meters
 
 class Point(object):
+    """A point on the face of the earth"""
     def __init__(self, latitude, longitude):
         self._lat = latitude
         self._long = longitude
@@ -18,14 +19,17 @@ class Point(object):
             raise IndexError('Point objects can only have two coordinates')
 
     def __iter__(self):
+        """Return an iterator containing the lat and long"""
         return iter([self.lat, self.long])
 
     @property
     def lat(self):
+        """Return the latitude in degrees"""
         return self._lat
 
     @property
     def long(self):
+        """Return the longitude in degrees"""
         return self._long
 
     def _to_radians(self, degrees):
@@ -33,13 +37,16 @@ class Point(object):
 
     @property
     def lat_radians(self):
+        """Return the latitude in radians"""
         return self._to_radians(self.lat)
 
     @property
     def long_radians(self):
+        """Return the longitude in radians"""
         return self._to_radians(self.long)
 
     def distance_to(self, point):
+        """Return the distance between two points in meters"""
         angle = math.acos(
                 sin(self.lat_radians) * sin(point.lat_radians) +
                 cos(self.lat_radians) * cos(point.lat_radians) *
