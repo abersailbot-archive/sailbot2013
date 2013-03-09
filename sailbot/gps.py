37e137c85d316dbdcf0744d6f226b0b0e2134eac
Louis Taylor <kragniz@gmail.com>
2013-03-09 04:12:29 +0000

    Added degree/minute -> decimal degree converter

--- sailbot/gps.py
+++ sailbot/gps.py
@@ -2,6 +2,14 @@ from point import Point
 import config
 import serial
 
+class AttributeDict(dict):
+        """Access elements of the dict as attributes"""
+        def __getattr__(self, attr):
+            return self[attr]
+
+        def __setattr__(self, attr, value):
+            self[attr] = value
+
 class Gps(object):
     def __init__(self):
         #self._gpsSerial = serial.Serial(config.gpsSerialport, 4800, timeout=0.5)
@@ -9,14 +17,38 @@ class Gps(object):
 
     def get_coords(self):
         """Return the current coordinates from the GPS"""
-        #response = self._gpsSerial.readline(None)
+        #line = self._gpsSerial.readline(None)
         pass
 
-    def _split_response(self, response):
-        r = response
-        return r[r.find('$')+1:r.find('*')].split(',')
+    def _parse_degrees(self, degrees):
+        pointIndex = degrees.find('.') - 2
+        return (
+                float(degrees[:pointIndex]) +
+                float(degrees[pointIndex:]) / 60
+               )
+
+
+    def _name_fields(self, line):
+        fields = line[line.find('$')+1:line.find('*')].split(',')[1:8]
+        names = [
+                    'time',
+                    'lat',
+                    'lat_direction',
+                    'long',
+                    'long_direction',
+                    'fix_quality',
+                    'satellite_number',
+                    'hdop',
+                    'altitude'
+                ]
+        d = AttributeDict()
+        for i in range(len(fields)):
+            print i, names[i], fields[i]
+            d[names[i]] = fields[i]
+        return d
 
 if __name__ == '__main__':
-    demoresponse = "$GPGGA,113245.000,5223.9915,N,00352.1781,W,1,08,1.0,329.0,M,50.9,M,,0000*4A"
+    demoline = "$GPGGA,113245.000,5223.9915,N,00352.1781,W,1,08,1.0,329.0,M,50.9,M,,0000*4A"
     gps = Gps()
-    print gps._split_response(demoresponse)
+    f=gps._name_fields(demoline)
+    print gps._parse_degrees(f.lat)
