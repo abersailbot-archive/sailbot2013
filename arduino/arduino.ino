6f1b8475d39b1d679e36008fbb26fcd0bfdfe612
Louis Taylor <kragniz@gmail.com>
2013-04-22 19:35:52 +0100

    Tidied up formatting of arduino code

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -8,6 +8,7 @@ Servo myRudderServo; // create servo object to control a servo
 Servo mySailServo; // a maximum of eight servo objects can be created 
 
 char inData[6]; // Allocate some space for the string
+int DEBUG = 0;
 
 void setup() {
     Serial.begin(9600); //Begin at 9600
@@ -19,49 +20,46 @@ void setup() {
     pinMode(11, INPUT);  //Use pinMode for setting up connection to wind sensor
     pinMode(12, OUTPUT);
     Wire.begin(); // Initialize the I2C bus for the compass
-    Serial.write("Power On\n");
+
+    if (DEBUG) {
+        Serial.write("Power On\n");
+    }
 }
 
 void getData() {
     char inChar=-1; // Where to store the character read
     byte index = 0; // Index into array; where to store the character
-    for(index = 0; index<5; index++)
-    {
-      while (Serial.available() == 0);
-      inChar = Serial.read(); // Read a character
-      if(inChar != '\n'){
-        inData[index] = inChar; // Store it
-      }
-      else{
-        break;
-      } 
+    for(index = 0; index<5; index++) {
+        while (Serial.available() == 0);
+        inChar = Serial.read(); // Read a character
+        if(inChar != '\n') {
+            inData[index] = inChar; // Store it
+        } else {
+            break;
+        }
     }
     inData[index] = '\0'; // Null terminate the string
 }
 
 void setServo(char servoChar, int turnAmount){
-    if('R' == servoChar){
-      turnAmount = constrain(turnAmount, 1000, 2000);
-      myRudderServo.writeMicroseconds(turnAmount);
-      //myRudderServo.writeMicroseconds(500);
+    if('R' == servoChar) {
+        turnAmount = constrain(turnAmount, 1000, 2000);
+        myRudderServo.writeMicroseconds(turnAmount);
     }
-    else{
-      turnAmount = constrain(turnAmount, 1000, 1850);
-      mySailServo.writeMicroseconds(turnAmount);
-      //mySailServo.writeMicroseconds(500);
+    else {
+        turnAmount = constrain(turnAmount, 1000, 1850);
+        mySailServo.writeMicroseconds(turnAmount);
     }
 }
 
-int getAmount(){
- // char newArray[5] = {
- //     inData[1], inData[2], inData[3], inData[4], 0    };
+int getAmount() {
     int turnAmount;
     turnAmount = (int) strtol(inData+1, NULL, 10);
     Serial.println(turnAmount);
     return turnAmount;
 }
 
-int readCompass(){
+int readCompass() {
     byte highByte, lowByte;
 
     Wire.beginTransmission(HMC6343_ADDRESS); // Start communicating with the HMC6343 compasss
@@ -78,7 +76,7 @@ int readCompass(){
     return (int)heading; // Print the sensor readings to the serial port.
 }
 
-int readWindSensor(){
+int readWindSensor() {
     int windSensorPin = 11;
     int pulseLength=0;
     int windAngle=0;
@@ -88,29 +86,26 @@ int readWindSensor(){
     return (windAngle);
 }
 
-void loop()
-{
+void loop() {
     getData();
-    //inData[0] = 'c'; // Infinate Compass Read
     switch(inData[0]){
-    case 'c' : 
-      Serial.println(readCompass()); //Compass Read
-      break;
-    case 'w' : 
-      Serial.println(readWindSensor()); //Wind Sensor Read
-      break;
-    case 'r' : 
-      Serial.println("r"); 
-      setServo('R', getAmount());// Rudder Set
-
-      break;
-    case 's' : 
-      Serial.println("s"); 
-      setServo('S', getAmount()); // Sail Set
-
-      break;
+        case 'c:
+            Serial.println(readCompass()); //Compass Read
+            break;
+        case 'w':
+            Serial.println(readWindSensor()); //Wind Sensor Read
+            break;
+        case 'r':
+            if (DEBUG) {
+              Serial.println("r");
+            }
+            setServo('R', getAmount()); // Rudder Set
+            break;
+        case 's':
+            if (DEBUG) {
+              Serial.println("s");
+            }
+            setServo('S', getAmount()); // Sail Set
+            break;
     }
 }
-
-
-
